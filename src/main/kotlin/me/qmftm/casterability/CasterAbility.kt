c2b40996610511f2706ef19fa5461c04c7ebd1a5
qmftm <blue00screenn00@gmail.com>
2026-05-19 00:10:51 +0900

    add new project

--- src/main/kotlin/me/qmftm/casterability/CasterAbility.kt
+++ src/main/kotlin/me/qmftm/casterability/CasterAbility.kt
@@ -5,10 +5,10 @@ import org.bukkit.plugin.java.JavaPlugin
 class CasterAbility : JavaPlugin() {
 
     override fun onEnable() {
-        // Plugin startup logic
+        // Plugin startup
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        // Plugin shutdown
     }
 }
